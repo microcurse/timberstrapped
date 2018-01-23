97b9e2ddc1a9d09c91cbff04e3181517d84b1216
microcurse <marc.maninang@gmail.com>
2018-01-23 10:50:46 -0800

    Added bootstrap and sass files and folder structures. Also fixed gulp watch to watch for files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
-var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
- 
-    gulp.src('./css/src/*.scss')
- 
-        .pipe(sass())
- 
-        .pipe(gulp.dest('./css'));
- 
+// Include necessary modules
+var gulp = require('gulp');
+    browserSync = require('browser-sync');
+    sass = require('gulp-sass');
+
+// Configure Browsersync.
+gulp.task('browser-sync', function() {
+    var files = [
+        './style.css',
+        './*.php'
+    ];
+
+    // Initialize Browsersync with a PHP server.
+    browserSync.init(files, {
+        proxy: "http://localhost:8888/portfolio-site/"
+    });
 });
- 
-gulp.task('default', ['sass']);
+
+// Configure Sass task to run when the specificied .scss files change.
+// Browsersync will also reload browsers.
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass({
+            'outputStyle': 'compressed'
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
+});
+
+// Create the default task that can be called using 'gulp'.
+// The task will process sass, run browser-sync and start watching for changes.
+gulp.task('default', ['sass','browser-sync'], function() {
+    gulp.watch("sass/**/*.scss", ['sass']);
+})
